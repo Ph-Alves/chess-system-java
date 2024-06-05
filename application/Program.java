3699494c6b8892c81bdb462d17b976355f8a797a
Ph-Alves <ph.costaalves@gmail.com>
2024-06-05 14:43:44 -0300

    Mudança de nome da classe main (Program)

--- application/Program.java
+++ application/Program.java
@@ -3,7 +3,7 @@ package chessSystem.application;
 import java.util.Scanner;
 import java.util.Locale;
 
-public class ClassName {
+public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
