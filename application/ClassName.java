f3f3d87fe289bd84bf28883f3b10e4a635357fe4
Ph-Alves <ph.costaalves@gmail.com>
2024-06-05 14:40:45 -0300

    Project created

---
+++ application/ClassName.java
@@ -0,0 +1,13 @@
+package chessSystem.application;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ClassName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        
+        sc.close();
+    }
+}
