396e7787d45f695383c0c170a2f1b2b94ced0e2f
Ph-Alves <ph.costaalves@gmail.com>
2024-07-04 13:26:07 -0300

    Fix - better treatment to promotion

--- CHESSSYSTEM/src/chess/ChessMatch.java
+++ CHESSSYSTEM/src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package src.chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -120,7 +119,7 @@ public class ChessMatch {
         if (promoted == null) {
             throw new IllegalStateException("There is no piece to be promoted");
         } else if (!type.equals("B") && !type.equals("N") && !type.equals("Q") && !type.equals("R")) {
-            throw new InvalidParameterException("Invalid type for promotion!");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
